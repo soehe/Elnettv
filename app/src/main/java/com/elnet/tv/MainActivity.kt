57a4aed3f9e68a94782eed389b92466a7a869835
Soehe <133252620+soehe@users.noreply.github.com>
2026-09-21 00:55:47 +0700

    Fix TV app startup and HLS playback

--- app/src/main/java/com/elnet/tv/MainActivity.kt
+++ app/src/main/java/com/elnet/tv/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.elnet.tv
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
@@ -13,9 +16,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val playerView = findViewById<PlayerView>(R.id.player_view)
         player = ExoPlayer.Builder(this).build().also { exoPlayer ->
             playerView.player = exoPlayer
+            exoPlayer.addListener(object : Player.Listener {
+                override fun onPlayerError(error: PlaybackException) {
+                    Toast.makeText(this@MainActivity, "Gagal memutar siaran", Toast.LENGTH_LONG).show()
+                }
+            })
             exoPlayer.setMediaItem(MediaItem.fromUri(demoUrl))
             exoPlayer.prepare()
             exoPlayer.playWhenReady = true
